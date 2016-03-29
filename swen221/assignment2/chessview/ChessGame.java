058df62c91a489c031b4d6cf8fad68c470f4182e
Felix Yan <286589258@qq.com>
2016-03-29 14:00:31 +1300

    Modify the text format of game-001.txt and game-002.txt

--- swen221/assignment2/chessview/ChessGame.java
+++ swen221/assignment2/chessview/ChessGame.java
@@ -10,9 +10,9 @@ import java.io.*;
 /**
  * This class represents a game of chess, which is essentially just a list moves
  * that make up the game.
- * 
+ *
  * @author djp
- * 
+ *
  */
 public class ChessGame {
 	private ArrayList<Round> rounds;
@@ -20,20 +20,20 @@ public class ChessGame {
 	public ChessGame(String sheet) throws IOException {
 		this(new StringReader(sheet));
 	}
-	
+
 	/**
 	 * Construct a ChessGame object from a given game sheet, where each round
 	 * occurs on a new line.
-	 * 
+	 *
 	 * @param gameSheet
 	 */
 	public ChessGame(Reader input) throws IOException {
 		rounds = new ArrayList<Round>();
-		
+
 		BufferedReader reader = new BufferedReader(input);
-		
+
 		// First, read in the commands
-		String line;		
+		String line;
 		while((line = reader.readLine()) != null) {
 			if(line.equals("")) { continue; } // skip blank lines
 			int pos = line.indexOf(' ');
@@ -50,11 +50,11 @@ public class ChessGame {
 	public List<Round> rounds() {
 		return rounds;
 	}
-	
+
 	/**
 	 * This method computes the list of boards which make up the game. If an
 	 * invalid move, or board is encountered then a RuntimeException is thrown.
-	 * 
+	 *
 	 * @return
 	 */
 	public List<Board> boards() {
@@ -62,15 +62,15 @@ public class ChessGame {
 		Board b = new Board();
 		boards.add(b);
 		boolean lastTime = false;
-		for(Round r : rounds) {		
+		for(Round r : rounds) {
 			if (lastTime) { return boards; }
-			b = new Board(b);			
+			b = new Board(b);
 			if(!b.apply(r.white())) { return boards; }
-			boards.add(b);			
+			boards.add(b);
 			if(r.black() != null) {
-				b = new Board(b);				
+				b = new Board(b);
 				if(!b.apply(r.black())) { return boards; }
-				boards.add(b);				
+				boards.add(b);
 			} else {
 				lastTime = true;
 			}
@@ -80,15 +80,15 @@ public class ChessGame {
 
 	/**
 	 * Construct a move object from a given string.
-	 * 
+	 *
 	 * @param str
 	 * @return
 	 */
 	private static Move moveFromString(String str, boolean isWhite) {
-		Piece piece;		
+		Piece piece;
 		int index = 0;
 		char lookahead = str.charAt(index);
-		
+
 		switch(lookahead) {
 			case 'N':
 				piece = new Knight(isWhite);
@@ -125,8 +125,8 @@ public class ChessGame {
 		Position start = positionFromString(str.substring(index,index+2));
 		char moveType = str.charAt(index+2);
 		Piece target = null;
-		index = index + 3;		
-		
+		index = index + 3;
+
 		if(moveType == 'x') {
 			lookahead = str.charAt(index);
 			switch(lookahead) {
@@ -151,23 +151,23 @@ public class ChessGame {
 					index++;
 					break;
 				default:
-					target = new Pawn(!isWhite); 
+					target = new Pawn(!isWhite);
 			}
 		} else if(moveType != '-') {
 			throw new IllegalArgumentException("invalid sheet");
 		}
-		
+
 		Position end = positionFromString(str.substring(index,index+2));
 		index = index + 2;
-		
+
 		Move move;
-		
+
 		if(target != null) {
 			move = new SinglePieceTake(piece,target,start,end);
 		} else {
 			move = new SinglePieceMove(piece,start,end);
 		}
-		
+
 		if((index+1) < str.length() && str.charAt(index) == 'e' && str.charAt(index+1) == 'p') {
 			move = new EnPassant((SinglePieceMove) move);
 			index+=2;
@@ -176,19 +176,19 @@ public class ChessGame {
 			Piece promotion;
 			switch(lookahead) {
 				case 'N':
-					promotion = new Knight(isWhite);					
+					promotion = new Knight(isWhite);
 					break;
 				case 'B':
-					promotion = new Bishop(isWhite);					
+					promotion = new Bishop(isWhite);
 					break;
 				case 'R':
-					promotion = new Rook(isWhite);					
+					promotion = new Rook(isWhite);
 					break;
 				case 'K':
-					promotion = new King(isWhite);					
+					promotion = new King(isWhite);
 					break;
 				case 'Q':
-					promotion = new Queen(isWhite);					
+					promotion = new Queen(isWhite);
 					break;
 				default:
 					throw new IllegalArgumentException("invalid sheet");
@@ -196,7 +196,7 @@ public class ChessGame {
 			move = new PawnPromotion((SinglePieceMove) move,promotion);
 			index+=2;
 		}
-		
+
 		if(index < str.length() && str.charAt(index) == '+') {
 			move = new Check((MultiPieceMove) move);
 			index++;
@@ -205,12 +205,15 @@ public class ChessGame {
 		}
 
 		if(index != str.length()) {
+			System.out.println("index: "+index);//debug
+			System.out.println("String is: "+str);//debug
+			System.out.println("String length is: "+str.length());
 			throw new IllegalArgumentException("invalid sheet");
-		}					
-		
+		}
+
 		return move;
 	}
-	
+
 	private static Position positionFromString(String pos) {
 		if(pos.length() != 2) {
 			throw new IllegalArgumentException("invalid position: " + pos);
